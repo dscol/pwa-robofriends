939447b4fcd8cc8ea4d05dcea5fc9cc592fdcc80
Derek <derekscolnick@gmail.com>
2020-12-19 21:53:00 +1000

    Updates

---
+++ precache-manifest.8baa9be32a32acb671008a29cce59bad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6190e8df7f02b9df84687fed93b269",
+    "url": "/dscol/pwa-robofriends.git/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/dscol/pwa-robofriends.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d1e3938d753d6dbe61ca",
+    "url": "/dscol/pwa-robofriends.git/static/css/main.a4397ecd.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/dscol/pwa-robofriends.git/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dscol/pwa-robofriends.git/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e3938d753d6dbe61ca",
+    "url": "/dscol/pwa-robofriends.git/static/js/main.fa301676.chunk.js"
+  },
+  {
+    "revision": "8bfdaf2c86f6a9a3140a",
+    "url": "/dscol/pwa-robofriends.git/static/js/runtime-main.83e37fa7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/dscol/pwa-robofriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
